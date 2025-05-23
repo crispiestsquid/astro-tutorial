77fedefd8697aed6d954453dfbafb059082b3cb5
Kevin Little <crispiestsquid@aol.com>
2025-05-22 20:29:36 -0400

    my first island!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://kaleidoscopic-gingersnap-4a85af.netlify.app/',
+  site: 'https://kaleidoscopic-gingersnap-4a85af.netlify.app/',
+  integrations: [preact()],
 });
